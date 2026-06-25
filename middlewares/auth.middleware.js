9a03a0984359b06d00549387d28741836241f4d7
unknown <domoracas@gmail.com>
2026-06-24 23:36:11 -0600

    Mostrar PDF

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,18 +3,19 @@ const jwt = require('jsonwebtoken');
 // Verifica que el token exista y sea valido
 function verificarToken(req, res, next) {
   const authHeader = req.headers['authorization'];
+  let token;
 
-  if (!authHeader) {
-    return res.status(401).json({ error: 'No se proporciono un token' });
+  if (authHeader) {
+    // El header viene como "Bearer eyJhbGc..." -> separamos la palabra "Bearer"
+    token = authHeader.split(' ')[1];
+  } else if (req.query.token) {
+    // Para casos como <iframe> o <img>, donde no se pueden mandar headers
+    token = req.query.token;
   }
-
-  // El header viene como "Bearer eyJhbGc..." -> separamos la palabra "Bearer"
-  const token = authHeader.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ error: 'Formato de token invalido' });
+    return res.status(401).json({ error: 'No se proporciono un token' });
   }
-
   jwt.verify(token, process.env.JWT_SECRET, (err, datosUsuario) => {
     if (err) {
       return res.status(403).json({ error: 'Token invalido o expirado' });
